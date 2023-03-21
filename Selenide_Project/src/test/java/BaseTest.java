b640345c93e9ebb0545a933c49998bf7163d3582
Stas Blashchuk <blashchukstas@gmail.com>
2023-03-21 17:17:14 +0200

    few more changes

--- Selenide_Project/src/test/java/BaseTest.java
+++ Selenide_Project/src/test/java/BaseTest.java
@@ -1,10 +1,7 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import org.example.MainPage;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.reporters.jq.Main;
-
 import static com.codeborne.selenide.Selenide.open;
 
 public class BaseTest {
@@ -14,6 +11,7 @@ public class BaseTest {
         Configuration.timeout = 10;
         open("https://www.yakaboo.ua/");
     }
+
     @AfterMethod
     public void tearDown() {
         Selenide.closeWebDriver();
